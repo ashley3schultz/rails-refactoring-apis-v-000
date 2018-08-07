51e1a7d2110133bd7b8097c36a95ae1d13a10604
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 17:37:21 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,5 @@
 class RepositoriesController < ApplicationController
   def index
-    gh = GithubService.new({"access_token" => session[:token]})
     github = GithubService.new({"access_token" => session[:token]})
     # response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     @repos_array = github.get_repos
