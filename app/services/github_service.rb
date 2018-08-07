047f9bbc04896b5327d7289de08d56bc612e9c2b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 17:20:20 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -12,7 +12,6 @@ class GithubService
         {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     @access_token = access_hash["access_token"]
-    binding.pry
   end
 
   def get_username
